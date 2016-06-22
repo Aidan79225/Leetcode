6ae91e2801768f115a3c6c7e5f0e3bcedd275781
Aidan <aidan79225@gmail.com>
2016-06-23 01:44:01 +0800

    no message

--- Medium/69. Sqrt(x).cpp
+++ Medium/69. Sqrt(x).cpp
@@ -3,9 +3,9 @@ public:
     int mySqrt(int x) {
         int r=x,l=0,mid,temp;
         if(x==1)return 1;
-        if(r>46340)r=46341;
-        while(r>l){
-            mid = (r+l)/2;
+        if(r>46340)r=46341;					//int限制最大2147483647 最大sqrt(x)=46341
+        while(r>l){							
+            mid = (r+l)/2;					//2分逼近法
             temp =mid*mid;
             if(temp>x){
                 r =mid;
