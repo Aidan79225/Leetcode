5d2683a49eef7782caf2a1632b2cf4a52506aedd
Aidan79225 <aidan79225@gmail.com>
2016-08-01 14:42:24 +0800

    Update 375. Guess Number Higher or Lower II.cpp

--- Medium/375. Guess Number Higher or Lower II.cpp
+++ Medium/375. Guess Number Higher or Lower II.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    int maxnum(int a,int b){
-        return a>b?a:b;
-    }
-    int minnum(int a,int b){
-        return a<b?a:b;
-    }
-    
-    
-    int payArrayAns(int min,int max,vector<vector<int>>& payArray){
-        if(max<=min)return 0;
-        if(payArray[min][max] !=0)return payArray[min][max];
-        if((max-min)<=1){
-            return min;
+
+    int cost(int left,int right,vector<vector<int>>& costTemp){
+        if(right-left==1)return left;
+        else if(right-left < 1)return 0;
+        else if(costTemp[left][right] !=0)return costTemp[left][right];
+        int min = 2147483647;
+        for(int i = right-1;i >= left+1; i--){
+            int temp =i+ max(cost(left,i-1,costTemp),cost(i+1,right,costTemp));
+            if(min> temp)
+                min = temp;
         }
-        else{
-            int maxPay=2147483647;
-            for(int i=min;i<max;i++){
-                int temp = i+ maxnum(payArrayAns(min,i-1,payArray),payArrayAns(i+1,max,payArray));
-                maxPay = minnum(maxPay,temp);
-            }
-            payArray[min][max] = maxPay;
-            return maxPay;
-        }
-    }
-    int pay(int min,int max){
-        if(max<=min)return 0;
-        if((max-min)<=1){
-            return min;
-        }
-        else{
-            int maxPay=2147483647;
-            for(int i=min;i<max;i++){
-                int temp = i+ maxnum(pay(min,i-1),pay(i+1,max));
-                maxPay = minnum(maxPay,temp);
-            }
-            return maxPay;
-        }
+        costTemp[left][right] = min;
+        return min;
     }
     int getMoneyAmount(int n) {
-        vector<vector<int>> payArray(n + 1, vector<int>(n + 1, 0));
-        return payArrayAns(1,n,payArray);
+        vector<vector<int>> costTemp(n + 1, vector<int>(n + 1, 0));
+        return cost(1,n,costTemp);
     }
-    
 };
