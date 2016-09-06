95ed911e3c106fe40d79fa9c12cd5722968ac4cc
Aidan79225 <aidan79225@gmail.com>
2016-09-06 21:28:24 +0800

    Create 389. Find the Difference.cpp

---
+++ Easy/389. Find the Difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for(int i = 0; i<s.size();i++){
+            sum -= s[i];
+        }
+        for(int i = 0; i<t.size();i++){
+            sum += t[i];
+        }
+        return sum;
+    }
+};
