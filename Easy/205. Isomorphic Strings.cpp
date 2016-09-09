ea1a0f597ef4093b4c24f8b2bf607c4cdf688b15
Aidan79225 <aidan79225@gmail.com>
2016-09-09 14:11:11 +0800

    Create 205. Isomorphic Strings.cpp

---
+++ Easy/205. Isomorphic Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> m1;
+        map<char, char> m2;
+        for(int i=0; i<s.size(); ++i){
+            if(m1.find(s[i])==m1.end() && m2.find(t[i])==m2.end()){
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            }
+            else if(m1.find(s[i])!=m1.end() && m2.find(t[i])!=m2.end()){
+                if(m1[s[i]]!=t[i] || m2[t[i]]!=s[i]){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+    return true;
+    }
+};
