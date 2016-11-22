97370b2de8bfe3bdfbf6c3e1000aa5bcbe71ddd6
Aidan79225 <aidan79225@gmail.com>
2016-11-22 10:39:13 +0800

    Update 80. Remove Duplicates from Sorted Array II.cpp

--- Medium/80. Remove Duplicates from Sorted Array II.cpp
+++ Medium/80. Remove Duplicates from Sorted Array II.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if(nums.size()==0)return 0;
-        int count =1;
+        bool count = false;
         for(int i =0;i<nums.size()-1;i++){
             if(nums[i] == nums[i+1]){
-                if(count >= 2){
+                if(count){
                     nums.erase(nums.begin()+i);
                     i--;
                 }else{
-                    count++;
+                    count = true;
                 }
             }else{
-                count = 1;
+                count = false;
             }
         }
         return nums.size();
