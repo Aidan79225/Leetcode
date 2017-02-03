6bbec5e06631b05b7ae90162557b74cecae1ddd2
Aidan79225 <aidan79225@gmail.com>
2017-02-03 16:38:52 +0800

    Update 75. Sort Colors.cpp

--- Medium/75. Sort Colors.cpp
+++ Medium/75. Sort Colors.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int left = -1, right = nums.size(), i = 0; //用left記錄最靠右邊的0,用right記錄最靠左邊的2
+        while(i < right){
+            if(nums[i] == 0 && i != ++left)
+                swap(nums[left], nums[i]);         //搬動0到該在的地方
+            else if(nums[i] == 2 && i != --right)
+                swap(nums[right], nums[i]);        //搬動2到該在的地方
+            else
+                i++;                               //遇到1不管他 看下個東西是啥
+        }      
+    }
+    void swap(int& a, int& b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    void sortColors1(vector<int>& nums) {
         int count[3] = {0};           //分別用來記錄0,1,2出現的次數
         for(int i=0;i<nums.size();i++){
             count[nums[i]]++;
