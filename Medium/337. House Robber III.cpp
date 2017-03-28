d3c91895f6dae5445d68716921a4638a06e30015
Aidan79225 <aidan79225@gmail.com>
2017-03-28 14:20:44 +0800

    Update 337. House Robber III.cpp

--- Medium/337. House Robber III.cpp
+++ Medium/337. House Robber III.cpp
@@ -10,6 +10,7 @@
 class Solution {
 public:
     int rob(TreeNode* root) {
+        return mRob(root).first;
         return rob(root,true,0);
     }
     int rob(TreeNode* root, bool can, int now){
@@ -22,4 +23,17 @@ public:
         notSteal = rob(root -> left, true, 0) +  rob(root -> right, true, 0) + now;
         return steal > notSteal ? steal : notSteal;
     }
+    pair<int, int> mRob(TreeNode* root){
+        pair<int, int> dp = make_pair(0,0);
+        if(root){
+            pair<int, int> left = mRob(root->left);
+            pair<int, int> right = mRob(root->right);
+            dp.second = left.first + right.first;
+            dp.first = max(dp.second ,left.second + right.second + root->val);
+        }
+        return dp;
+    }
+    
 };
+
+
