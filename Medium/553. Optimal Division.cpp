0ac5a33dbbfbb60b2bd5180f277eef593eaf4751
Aidan79225 <aidan79225@gmail.com>
2017-06-05 16:08:03 +0800

    Update 553. Optimal Division.cpp

--- Medium/553. Optimal Division.cpp
+++ Medium/553. Optimal Division.cpp
@@ -10,11 +10,11 @@ public:
         }else if(nums.size() == 2){
             ans += "/";
         }
-        for(int i = 1;i<nums.size();i++){
+        for(int i = 1;i<nums.size()-1;i++){
             ans += to_string(nums[i]);
             ans += "/";
         }
-        if(nums.size()>1)ans = ans.substr (0,ans.length()-1);
+        if(nums.size()>1)ans += to_string(nums[nums.size()-1]);
         if(nums.size() > 2){
             ans += ")";
         }
