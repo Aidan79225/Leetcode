3221c8c6cf4199986cfdf6075356b23177d9e1bc
Aidan79225 <aidan79225@gmail.com>
2017-07-28 17:30:47 +0800

    Create 535. Encode and Decode TinyURL.cpp

---
+++ Easy/535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    unordered_map<string,string> mMap;
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
