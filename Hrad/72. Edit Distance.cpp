1f51f0c4dd0a800eb8e7da3f8178dab3fa5e06b9
Aidan79225 <aidan79225@gmail.com>
2017-11-09 02:54:17 -0600

    Update 72. Edit Distance.cpp

--- Hrad/72. Edit Distance.cpp
+++ Hrad/72. Edit Distance.cpp
@@ -5,21 +5,19 @@ public:
         int size2 = word2.size();
         if(size1 == 0)return size2;
         if(size2 == 0)return size1;
-        
-        vector<vector<int>> dp(size1,vector<int>(size2,0));
-        dp[0][0] = word1[0] == word2[0] ? 0 : 1;
-        for(int i = 1 ; i < size1 ; i++){
-            dp[i][0] = dp[i-1][0] + ( word1[i] == word2[0] ? 0 : 1 );
+        vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
+        for(int i = 0 ; i <= size1 ; i++){
+            dp[i][0] = i;
         }
-        for(int i = 1 ; i < size2 ; i++){
-            dp[0][i] = dp[0][i-1] + ( word1[0] == word2[i] ? 0 : 1 );
+        for(int i = 0 ; i <= size2 ; i++){
+            dp[0][i] = i;
         }
-        for(int i = 1 ; i < size1 ; i++){
-            for(int j = 1 ; j < size2 ; j++){
-                int diff = word1[i] == word2[j] ? 0 : 1;
-                dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1])) + diff;
+        for(int i = 1 ; i <= size1 ; i++){
+            for(int j = 1 ; j <= size2 ; j++){
+                int diff = word1[i-1] == word2[j-1] ? 0 : 1;
+                dp[i][j] = min(dp[i-1][j]+ 1 ,min(dp[i][j-1]+ 1,dp[i-1][j-1]+ diff)) ;
             }
         }
-        return dp[size1-1][size2-1];
+        return dp[size1][size2];
     }
 };
