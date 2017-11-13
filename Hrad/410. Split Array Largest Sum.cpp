6a00b9e6ab218d244c9345a2b035b7ae187538d1
Aidan79225 <aidan79225@gmail.com>
2017-11-13 00:54:52 -0600

    Update 410. Split Array Largest Sum.cpp

--- Hrad/410. Split Array Largest Sum.cpp
+++ Hrad/410. Split Array Largest Sum.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
+    using ll = long long;
+
+    bool canSplit(vector<int>& nums, int m, ll sum) {   // 把nums分成每堆剛好大於sum後 最多可以分成幾堆
+        int c = 1;
+        ll s = 0;
+        for (auto& num : nums) {
+            s += num;
+            if (s > sum) {
+                s = num;
+                ++c;
+            }
+        }
+        return c <= m;
+    }
+
     int splitArray(vector<int>& nums, int m) {
+        ll left = 0, right = 0;
+        for (auto& num : nums) {
+            left = max(left, (ll)num);      //最小值  = 分成nums.size()堆
+            right += num;                   //最大值
+        }
+        while (left <= right) {
+            ll mid = left + (right-left)/2;
+            if (canSplit(nums, m, mid))
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return left;
+    }
+    
+    int splitArrayDp(vector<int>& nums, int m) {
         int size = nums.size();
         vector<vector<int>> dp(m,vector<int>(size,0));
         dp[0][0] = nums[0];
