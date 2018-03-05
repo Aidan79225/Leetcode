d3df96f822d1a6f9f58805273d984df9276965ea
Aidan79225 <aidan79225@gmail.com>
2018-03-05 18:20:49 +0800

    Update 179. Largest Number.cpp

--- Medium/179. Largest Number.cpp
+++ Medium/179. Largest Number.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    static bool myCompare (string as, string bs){
-        return as + bs>bs + as;
-    }
-
     string largestNumber(vector<int>& nums) {
         string ans="";
         vector<string> mystring;
@@ -11,7 +7,9 @@ public:
         for(int i=0;i<nums.size();i++){
             mystring.push_back(to_string(nums[i]));
         }
-        sort(mystring.begin(),mystring.end(),myCompare);
+        sort(mystring.begin(),mystring.end(),[](string as, string bs){
+                    return as + bs>bs + as;
+        });
         for(int i=0;i<nums.size();i++){
             ans += mystring[i];
         }
