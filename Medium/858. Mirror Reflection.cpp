ee13b4aede24f27bb6c48401e1f89e268c0ecfd6
Aidan79225 <aidan79225@gmail.com>
2018-07-05 14:41:28 +0800

    Update 858. Mirror Reflection.cpp

--- Medium/858. Mirror Reflection.cpp
+++ Medium/858. Mirror Reflection.cpp
@@ -4,27 +4,10 @@ public:
         int lcm = p * q / gcd(q, p);
         int k = lcm / p;
         int n = lcm / q;
-        if(k & 0b1){
-            if(n & 0b1){
-                return 1;
-            }else{
-                return 2;
-            }
-        }else{
-            if(n & 0b1){
-                return 0;
-            }else{
-                return -1;
-            }
-        }
-        return lcm;
+        return (k & 0b1) ? (n & 0b1 ? 1 : 2) : (n & 0b1 ? 0 : -1);
     }
     int gcd(int a, int b){
-        if(a > b){
-            return gcd(b, a);
-        }
-        if(b % a == 0)return a;
-        else return gcd(b % a, a);
+        if(b)while((b %= a) && (a %= b));
+        return a + b;
     }  
-    
 };
