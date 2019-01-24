3ae3a5d4cf03eaabb8be5125a3f710abce5bdbf0
Aidan79225 <aidan79225@gmail.com>
2019-01-24 13:46:27 +0800

    Update 978. Longest Turbulent Subarray.cpp

--- Medium/978. Longest Turbulent Subarray.cpp
+++ Medium/978. Longest Turbulent Subarray.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     int maxTurbulenceSize(vector<int>& A) {
         int size = A.size();
+        int sign = -1;
+        int ans = 1;
+        int r = 1;
+        int l = 1;
+        for (int i = 0; i < size-1; i++) {
+            int cur = A[i]*sign;
+            int next = A[i+1]*sign;
+            if (cur > next) {
+                l++;
+                ans = max(ans, l);
+                r = 1;
+            } else if (cur < next) {
+                r++;
+                ans = max(ans, r);
+                l = 1;
+            } else {
+                r = 1;
+                l = 1;
+            }
+            sign = -sign;
+        }
+        return ans;
+    }
+    
+    int maxTurbulenceSize1(vector<int>& A) {
+        int size = A.size();
         vector<int> lis(size, 1);
         vector<int> rlis(size, 1);
         int sign = -1;
