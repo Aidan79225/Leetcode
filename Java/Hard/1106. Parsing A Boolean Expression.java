d0da05de65bdd76712820cb5eb4a8021ee7b9625
Aidan79225 <aidan79225@gmail.com>
2019-07-03 11:44:08 +0800

    Update 1106. Parsing A Boolean Expression.java

--- Java/Hard/1106. Parsing A Boolean Expression.java
+++ Java/Hard/1106. Parsing A Boolean Expression.java
@@ -1,77 +1,56 @@
 class Solution {
+    int cur = 0;
+    char[] expression;
     public boolean parseBoolExpr(String expression) {
-        return eval(expression.toCharArray(), 0, expression.length()-1);
+        cur = 0;
+        this.expression = expression.toCharArray();
+        return eval();
     }
     
-    private boolean eval(char[] expression, int cur, int end) {
+    private boolean eval() {
         switch(expression[cur]) {
             case 'f':
+                cur++;
                 return false;
             case 't':
+                cur++;
                 return true;
             case '!':
-                return !eval(expression, cur+2, end-1);
+                return not();
             case '&':
-                return and(expression, cur, end);
+                return and();
             case '|':
-                return or(expression, cur, end);
+                return or();
+            default :
+                cur++;
+                return eval();
         }
-        return false;
     }
     
-    private boolean and(char[] expression, int cur, int end) {
+    private boolean not() {
+        cur += 2;
+        boolean ans = !eval();
+        cur++;
+        return ans;
+    }
+    
+    private boolean and() {
         boolean temp = true;
-        int c = cur;
-        c += 2;
-        while (c < end && expression[c] != ')') {
-            int next = findEnd(expression, c);
-            temp &= eval(expression, c, next);
-            c = next+1;
-            if (expression[c] == ',') {
-                c++;
-            }
+        cur += 2;
+        while (expression[cur] != ')') {
+            temp &= eval();
         }
+        cur++;
         return temp;
     }
     
-    private boolean or(char[] expression, int cur, int end) {
+    private boolean or() {
         boolean temp = false;
-        int c = cur;
-        c += 2;
-        while (c < end && expression[c] != ')') {
-            int next = findEnd(expression, c);
-            temp |= eval(expression, c, next);
-            c = next+1;
-            if (expression[c] == ',') {
-                c++;
-            }
+        cur += 2;
+        while (expression[cur] != ')') {
+            temp |= eval();
         }
+        cur++;
         return temp;
-    }
-        
-    
-    private int findEnd(char[] expression, int s) {
-        switch(expression[s]) {
-            case '|':
-            case '!':
-            case '&':
-                s++;
-                int t = 0;
-                while(s < expression.length) {
-                    switch(expression[s]) {
-                        case '(':
-                            t++;
-                            break;
-                        case ')':
-                            t--;
-                            break;
-                    }
-                    if (t == 0) {
-                        return s;
-                    }
-                    s++;
-                }
-        }
-        return s;
     }
 }
