6e1384f6d2ed329e6110bf9666e28a48a3bbaf62
Aidan79225 <aidan79225@gmail.com>
2019-11-22 10:27:05 +0800

    Update 1143. Longest Common Subsequence.java

--- Java/Medium/1143. Longest Common Subsequence.java
+++ Java/Medium/1143. Longest Common Subsequence.java
@@ -1,16 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        int[][] dp = new int[2][text2.length() + 1];
+        int old = 0;
         for (int i = 0; i < text1.length(); i++) {
             char c1 = text1.charAt(i);
+            int newLine = 1 - old;
             for (int j = 0; j < text2.length(); j++) {
                 char c2 = text2.charAt(j);
-                dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                dp[newLine][j+1] = Math.max(dp[newLine][j], dp[old][j+1]);
                 if (c1 == c2) {
-                    dp[i+1][j+1] = Math.max(dp[i][j] + 1, dp[i+1][j+1]);
+                    dp[newLine][j+1] = Math.max(dp[old][j] + 1, dp[newLine][j+1]);
                 }
             }
+            old = 1 - old;
         }
-        return dp[text1.length()][text2.length()];
+        return dp[old][text2.length()];
     }
 }
